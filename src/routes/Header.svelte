9c8edf55343f930b5b4c93fffe1653414b047237
PlutoEx <arlan.kuralbayev2002@gmail.com>
2023-04-25 16:19:04 +0300

    Edited header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,34 +1,30 @@
 <script>
-	let isUser = false;
-	let dropdownOpen = false;
+	import Cookies from 'js-cookie';
 	let header;
 
-	function toggleDropdown() {
-		dropdownOpen = !dropdownOpen;
+	function handleSignOut() {
+		console.log(Cookies.get('token'));
+		Cookies.remove('token', {secure: true});
+		console.log(Cookies.get('token'));
+		window.location.href = '/login';
 	}
 </script>
 <header class="header-container" bind:this={header}>
 	<div class="header-left">
-		<img src="favicon.png" alt="Logo" class="header-logo" />
+		<img src="favicon.png" alt="Logo" class="header-logo"/>
 		<span class="header-title">Money Management</span>
 	</div>
 	<nav class="header-nav">
 		<a href="/" class="header-link">Home</a>
-		<a href="/profile" class="header-link">Profile</a>
-		<div class="header-dropdown" on:click={toggleDropdown}>
-			<span class="header-link">Transactions</span>
-			{#if dropdownOpen}
-				<div class="header-dropdown-menu">
-					<a href="/add" class="header-dropdown-link">Add</a>
-					<a href="/history" class="header-dropdown-link">History</a>
-				</div>
-			{/if}
-		</div>
+		{#if Cookies.get('token')}
+			<a href="/profile" class="header-link">Profile</a>
+			<a href="/history" class="header-link"> Transactions </a>
+		{/if}
 		<a href="/about" class="header-link">About</a>
 	</nav>
 	<div class="header-right">
-		{#if isUser}
-			<button class="header-button" on:click={alert('No function')}>Sign Out</button>
+		{#if Cookies.get('token')}
+			<button class="header-button" on:click={handleSignOut}>Sign Out</button>
 		{:else}
 			<a href="/login" class="header-button">Sign In</a>
 		{/if}
@@ -50,8 +46,6 @@
 	.header-left {
 		display: flex;
 		align-items: center;
-		/*font-family: ;*/
-		/*font-size: 20px;*/
 	}
 
 	.header-logo {
@@ -69,6 +63,10 @@
 		align-items: center;
 	}
 
+	button {
+		all: unset;
+	}
+
 	.header-link {
 		margin-left: 24px;
 		color: silver;
@@ -78,35 +76,6 @@
 
 	.header-link:hover {
 		text-decoration: underline;
-	}
-
-	.header-dropdown {
-		position: relative;
-	}
-
-	.header-dropdown-menu {
-		position: absolute;
-		width: 80%;
-		left: 18px;
-		z-index: 1;
-		background-color: #333333;
-		border: 1px solid gray;
-		padding: 5px;
-		margin-top: 5px;
-		border-radius: 10px;
-		gap: 5px;
-	}
-
-	.header-dropdown-link {
-		display: block;
-		color: silver;
-		text-decoration: none;
-		padding: 5px;
-	}
-
-	.header-dropdown-link:hover {
-		background-color: #222222;
-		border-radius: 5px;
 	}
 
 	.header-button {
